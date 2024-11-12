0da3d9e900dc148b21d8a9cfc716b1f19443d4fe
kadir-zeren <k4dzeren@gmail.com>
2024-11-12 13:18:37 +0300

    multi condition

--- conditions.js
+++ conditions.js
@@ -45,13 +45,37 @@ console.log(String(myNumber3));
 const calismaSuresi = prompt("Calisma süresi:");
 let maas = prompt("Maasini giriniz:");
 
-if (calismaSuresi >= 10) {
-//   maas = Math.trunc(maas * 1.1);
-  maas = Math.round(maas * 1.1);
-  console.log("Zamli maasiniz:", maas);
-  console.log(`Zamli maasiniz: ${maas}`);
-} else {
-  console.log("Uzgunuz maasinaza zam yapamayirouz", maas);
-}
+//? 2 kosullu
+// if (calismaSuresi >= 10) {
+// //   maas = Math.trunc(maas * 1.1);
+//   maas = Math.round(maas * 1.1);
+//   console.log("Zamli maasiniz:", maas);
+//   console.log(`Zamli maasiniz: ${maas}`);
+// } else {
+//   console.log("Uzgunuz maasinaza zam yapamayirouz", maas);
+// }
 
+// console.log("Güle güle");
+
+//? Tek kosullu
+if (calismaSuresi >= 10) {
+  maas = Math.round(maas * 1.1);
+  console.log(`Zamli maasiniz: ${maas}`);
+}
 console.log("Güle güle");
+
+//? Cok kosullu
+const grade = prompt("Please enter your grade:");
+
+let result = "";
+if (grade < 40) {
+  result = "FF";
+} else if (grade <= 50) {
+  result = "DD";
+} else if (grade <= 65) {
+  result = "CC";
+} else if (grade <= 89) {
+  result = "BB";
+} else {
+  result = "AA";
+}
