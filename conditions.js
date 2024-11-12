a0816f55c8031bdbf9f5cd498079e95f2a0d0591
kadir-zeren <k4dzeren@gmail.com>
2024-11-12 13:00:03 +0300

    string

--- conditions.js
+++ conditions.js
@@ -23,7 +23,20 @@ let myNumber2 = Number(myNumber);
 console.log(myNumber2); //? NaN
 console.log(typeof myNumber2); //? number
 
-const dolar = "1000";
-const tl = "2000";
+const dolar = "1000.3";
+const tl = "2000.2";
 const totalMoney = Number(dolar) + +tl;
 console.log(totalMoney);
+
+//* Alternatif olarak parseInt() ve parseFloat() da kullanılabilir.
+const dolarInt = parseInt(dolar); //? tamsayıya cevirir
+console.log(dolarInt);
+const tlFloat = parseFloat(tl); //? virgüllü sayıya çevirir
+console.log(tlFloat);
+
+console.log(parseFloat(null));
+console.log(parseFloat("")); //? NaN
+
+//? String
+const myNumber3 = 2000;
+console.log(String(myNumber3));
